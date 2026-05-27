d2ac756fdd33c16f72ee8d1cb3691b02efe64b4a
Luinil <61002225+Flolink1er@users.noreply.github.com>
2026-05-27 19:40:12 +0200

    Add Actualites and ActualiteInfo components, update routing and navbar links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { loggedInGuard } from 'models/guards/logged-in.guard';
 import { loggedOffGuard } from 'models/guards/logged-off.guard';
+import { ActualiteInfo } from 'pages/actualite-info/actualite-info';
+import { Actualites } from 'pages/actualites/actualites';
 import { Error } from 'pages/error/error';
 import { Home } from 'pages/home/home';
 import { Login } from 'pages/login/login';
@@ -22,6 +24,10 @@ export const routes: Routes = [
     canActivate: [loggedInGuard],
   },
   { path: 'services/:id', title: 'Services', component: ServiceInfo, canActivate: [loggedInGuard] },
+
+  { path: 'actualites', title: 'Actualités', component: Actualites, canActivate: [loggedInGuard]},
+  { path: 'actualites/:id', title: 'Actualités', component: ActualiteInfo, canActivate: [loggedInGuard]},
+
   { path: 'error', title: 'Error', component: Error },
   { path: '**', redirectTo: 'home' },
 ];
---
+++ src/app/pages/actualite-info/actualite-info.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actualite-info',
+  imports: [],
+  templateUrl: './actualite-info.html',
+  styleUrl: './actualite-info.css',
+})
+export class ActualiteInfo {}
